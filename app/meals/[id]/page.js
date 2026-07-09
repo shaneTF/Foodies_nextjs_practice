f49f25b5817a459fc0d86550de1c573f8752d8ff
Shane Franklin <shanefranklin148@gmail.com>
2026-07-09 15:41:39 -0400

    first commit

---
+++ app/meals/[id]/page.js
@@ -0,0 +1,7 @@
+export default function SpecificMeals({ params }) {
+  return (
+    <div>
+      <h1>Meals {params.id}</h1>
+    </div>
+  );
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,7 @@
+export default function ShareMeal() {
+  return (
+    <div>
+      <h1>Meals/Share</h1>
+    </div>
+  );
+}
