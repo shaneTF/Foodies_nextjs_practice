f60bd86cc5ef458f40ddd235030f0b98e081ef71
Shane Franklin <shanefranklin148@gmail.com>
2026-07-09 18:23:19 -0400

    Added specific meal pages with recipe

--- app/meals/[id]/page.js
+++ app/meals/[id]/page.js
@@ -1,7 +1,38 @@
-export default function SpecificMeals({ params }) {
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
+
+export default async function MealDetailsPage({ params }) {
+  const { id } = await params;
+  const meal = getMeal(id);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
-    <div>
-      <h1>Meals {params.id}</h1>
-    </div>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
   );
 }
